5933d605b055d66415f8714decc0fc96fecfe262
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-07-17 19:40:31 +0200

    Add image

--- src/ios/ActivityIndicator.m
+++ src/ios/ActivityIndicator.m
@@ -29,10 +29,6 @@
         self.activityIndicator.dimBackground = NO;
     }
     
-    
-    
-    // Check for types
-    
     if ([type isEqual: @"indeterminate-simple"]) {
         self.activityIndicator.mode = MBProgressHUDModeIndeterminate;
     }
@@ -94,6 +90,185 @@
 
 
 
+- (void)showSimple:(CDVInvokedUrlCommand *)command {
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    self.activityIndicator.mode = MBProgressHUDModeIndeterminate;
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+- (void)showSimpleWithLabel:(CDVInvokedUrlCommand *)command {
+    
+    // add functionality for color + font
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    NSString* text = [command.arguments objectAtIndex:1];
+
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    self.activityIndicator.mode = MBProgressHUDModeIndeterminate;
+    
+    self.activityIndicator.labelText = text;
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+}
+
+- (void)showSimpleWithLabelDetail:(CDVInvokedUrlCommand *)command   {
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    NSString* text = [command.arguments objectAtIndex:1];
+    NSString* detail = [command.arguments objectAtIndex:2];
+
+    
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    self.activityIndicator.mode = MBProgressHUDModeIndeterminate;
+    self.activityIndicator.labelText = text;
+    self.activityIndicator.detailsLabelText = detail;
+    
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+-(void)showDeterminate:(CDVInvokedUrlCommand *)command {
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    self.activityIndicator.mode = MBProgressHUDModeDeterminate;
+    
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+-(void)showDeterminateWithLabel:(CDVInvokedUrlCommand *)command {
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    NSString* text = [command.arguments objectAtIndex:1];
+
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    self.activityIndicator.mode = MBProgressHUDModeDeterminate;
+    self.activityIndicator.labelText = text;
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+
+- (void)showDeterminateAnnular:(CDVInvokedUrlCommand *)command  {
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    self.activityIndicator.mode = MBProgressHUDModeAnnularDeterminate;
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+- (void)showDeterminateBar:(CDVInvokedUrlCommand *)command {
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    self.activityIndicator.mode = MBProgressHUDModeDeterminateHorizontalBar;
+    
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+-(void)showSuccess:(CDVInvokedUrlCommand *)command {
+    bool dim = [[command.arguments objectAtIndex:0] boolValue];
+    
+    self.activityIndicator = nil;
+	self.activityIndicator = [MBProgressHUD showHUDAddedTo:self.webView.superview animated:YES];
+    
+    self.activityIndicator.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]];
+
+    
+    self.activityIndicator.mode = MBProgressHUDModeCustomView;
+    
+    
+    // Check for dim : true ? false
+    if (dim == true) {
+        self.activityIndicator.dimBackground = YES;
+    }
+    else if (dim == false) {
+        self.activityIndicator.dimBackground = NO;
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
 /**
  * This hide the ProgressDialog
  */
@@ -107,6 +282,12 @@
 	[self.activityIndicator hide:YES];
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+
+- (void)progressTask {
+    
 }
 
 @end
--- www/activityIndicator.js
+++ www/activityIndicator.js
@@ -1,18 +1,84 @@
 var ActivityIndicator = {
-  // 1. type    @string
-  // 2. dim     @bool
-  // 3. text    @string
-  // 4. detail  @string
-    show: function (type, dim, text, detail) {
+  show: function (type, dim, label, detail) {
+    dim = dim || false;
+    label = label || "Please wait...";
+    detail = detail || '';
 
-    	text = text || "Please wait...";
-      detail = detail || '';
+    cordova.exec(null, null, "ActivityIndicator", "show", [type, dim, label, detail]);
+  },
 
-        cordova.exec(null, null, "ActivityIndicator", "show", [type, dim, text, detail]);
-    },
-    hide: function () {
-        cordova.exec(null, null, "ActivityIndicator", "hide", []);
-    }
+
+  showSimple: function (dim) {
+    dim = dim || false;
+    cordova.exec(null, null, "ActivityIndicator", "showSimple", [dim]);
+
+  },
+
+  showSimpleWithLabel: function (dim, label) {
+    dim = dim || false;
+    label = label || "Please wait...";
+
+    cordova.exec(null, null, "ActivityIndicator", "showSimpleWithLabel", [dim, label]);
+  },
+
+  showSimpleWithLabelDetail: function (dim, label, detail) {
+    dim = dim || false;
+    label = label || "Please wait...";
+    detail = detail || "Loading";
+
+    cordova.exec(null, null, "ActivityIndicator", "showSimpleWithLabelDetail", [dim, label, detail]);
+  },
+
+  showDeterminate: function (dim) {
+    dim = dim || false;
+    cordova.exec(null, null, "ActivityIndicator", "showDeterminate", [dim]);
+  },
+
+  showDeterminateWithLabel: function (dim, label) {
+    dim = dim || false;
+    label = label || "Please wait...";
+
+    cordova.exec(null, null, "ActivityIndicator", "showDeterminateWithLabel", [dim, label]);
+  },
+
+  showDeterminateAnnular: function (dim) {
+    dim = dim || false;
+    cordova.exec(null, null, "ActivityIndicator", "showDeterminateAnnular", [dim]);
+  },
+
+  showDeterminateAnnularWithLabel: function (dim, label) {
+    dim = dim || false;
+    label = label || "Please wait...";
+
+    cordova.exec(null, null, "ActivityIndicator", "showDeterminateAnnularWithLabel", [dim, label]);
+  },
+
+  showDeterminateBar: function (dim) {
+    dim = dim || false;
+
+    cordova.exec(null, null, "ActivityIndicator", "showDeterminateBar", [dim]);
+  },
+
+  showDeterminateBarWithLabel: function (dim, label) {
+    dim = dim || false;
+    label = label || "Please wait...";
+
+    cordova.exec(null, null, "ActivityIndicator", "showDeterminateBarWithLabel", [dim, label]);
+  },
+
+  showSuccess: function (dim, label) {
+    dim = dim || false;
+    label = label || "Success";
+
+    cordova.exec(null, null, "ActivityIndicator", "showSuccess", [dim, label]);
+  },
+
+
+
+
+  hide: function () {
+    cordova.exec(null, null, "ActivityIndicator", "hide", []);
+  }
 };
 
 module.exports = ActivityIndicator;
